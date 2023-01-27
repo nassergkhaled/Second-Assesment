29056a55c01e21c3fc655ed0eeace2b4d2189c5a
Nasser Al-den Ghassan Shareef Khaled <nassergk1@gmail.com>
2023-01-27 18:30:35 +0200

    Read File

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File("input.txt"));
+        String input = scanner.nextLine();
+        scanner.close();
+
+        List<String> sequences = new ArrayList<>();
+        Matcher m = Pattern.compile("\\d+|\\D+").matcher(input);
+        while (m.find()) {
+            sequences.add(m.group());
+        }
+        System.out.println(input+"/n"+sequences);
+    }
+}
