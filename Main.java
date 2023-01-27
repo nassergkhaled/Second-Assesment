b58c857e10f2b372f4f4c459fa0ab6fb04189e62
Nasser Al-den Ghassan Shareef Khaled <nassergk1@gmail.com>
2023-01-27 18:59:45 +0200

    Final Commit

--- Main.java
+++ Main.java
@@ -19,20 +19,24 @@ public class Main {
         while (m.find()) {
             sequences.add(m.group());
         }
-        System.out.println(sequences);
 
         for (int i = 0; i < sequences.size(); i++) {
             char[] chars = sequences.get(i).toCharArray();
             if (Character.isDigit(chars[0])) {
-                Arrays.sort(chars);// Or i can use any customized sort algo like single-pivot-quick , merge , bubble sort but this function uses Dual-Pivot Quicksort
+                Arrays.sort(chars);// Or I can use any customized sort algo like single-pivot-quick , merge , bubble sort but this function uses Dual-Pivot Quicksort
             } else {
                 Arrays.sort(chars);
             }
             sequences.set(i, new String(chars));
         }
 
+        StringBuilder output = new StringBuilder(); // I used StingBuilder to access the append method since it does not exist in the String Class
+        for (String seq : sequences) {
+            output.append(seq);
+        }
+
         FileWriter writer = new FileWriter("my-output.txt");
-        writer.write(sequences.toString());
+        writer.write(output.toString());
         writer.close();
 
     }
